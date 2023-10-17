6c32d2d49b8ece1879d3a0bbb2e3987971918ab6
John Lu <johnlu2006@gmail.com>
2023-10-17 13:17:36 -0500

    use themes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-// import for NavBar and ApplicationView
+// imports for login props, app structure: NavBar and ApplicationView
 import { useEffect, useState } from "react";
 import { tryGetLoggedInUser } from "./managers/authManager";
 import NavBar from "./components/NavBar";
 import ApplicationViews from "./components/ApplicationViews";
 
+// imports or MUI and theme
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+
 
 function App() {
+  // themes and sidebar related state
+  const [theme, colorMode] = useMode();
+  // for loggedInUser cache
   const [loggedInUser, setLoggedInUser] = useState();
 
   useEffect(() => {
@@ -21,13 +28,18 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
-      <ApplicationViews
-        loggedInUser={loggedInUser}
-        setLoggedInUser={setLoggedInUser}
-      />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}> {/* MUI function: using custom themes */}
+        <CssBaseline /> {/* MUI function: return CSS to default */}
+        <div className="app">
+          <NavBar loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} />
+          <ApplicationViews
+            loggedInUser={loggedInUser}
+            setLoggedInUser={setLoggedInUser}
+          />
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
