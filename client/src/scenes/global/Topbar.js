94be58896bd6f9b202832d84365ac0cca5c26474
John Lu <johnlu2006@gmail.com>
2023-10-17 22:50:49 -0500

    add logout feature to Topbar

--- client/src/scenes/global/Topbar.js
+++ client/src/scenes/global/Topbar.js
@@ -1,3 +1,4 @@
+import { logout } from "../../managers/authManager.js";
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../../theme";
@@ -8,47 +9,60 @@ import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
+import LogoutIcon from '@mui/icons-material/Logout';
 
-const Topbar = () => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  const colorMode = useContext(ColorModeContext);
+const Topbar = ({ loggedInUser, setLoggedInUser }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
 
-  return (
-    <Box display="flex" justifyContent="space-between" p={2}>
-      {/* SEARCH BAR */}
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
-        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
-      </Box>
+    if (!loggedInUser) { return null };
 
-      {/* ICONS */}
-      <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
-          {theme.palette.mode === "dark" ? (
-            <DarkModeOutlinedIcon />
-          ) : (
-            <LightModeOutlinedIcon />
-          )}
-        </IconButton>
-        <IconButton>
-          <NotificationsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <SettingsOutlinedIcon />
-        </IconButton>
-        <IconButton>
-          <PersonOutlinedIcon />
-        </IconButton>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box display="flex" justifyContent="space-between" p={2}>
+            {/* SEARCH BAR */}
+            <Box
+                display="flex"
+                backgroundColor={colors.primary[400]}
+                borderRadius="3px"
+            >
+                <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+                <IconButton type="button" sx={{ p: 1 }}>
+                    <SearchIcon />
+                </IconButton>
+            </Box>
+
+            {/* ICONS */}
+            <Box display="flex">
+                <IconButton onClick={colorMode.toggleColorMode}>
+                    {theme.palette.mode === "dark" ? (
+                        <DarkModeOutlinedIcon />
+                    ) : (
+                        <LightModeOutlinedIcon />
+                    )}
+                </IconButton>
+                <IconButton>
+                    <NotificationsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlinedIcon />
+                </IconButton>
+                <IconButton>
+                    <PersonOutlinedIcon />
+                </IconButton>
+                <IconButton
+                    onClick={(e) => {
+                        e.preventDefault();
+                        logout().then(() => {
+                            setLoggedInUser(null);
+                        });
+                    }}
+                >
+                    <LogoutIcon />
+                </IconButton>
+            </Box>
+        </Box>
+    );
 };
 
 export default Topbar;
