dfb62eae16257ed282e44c677ac47e80652fb8a9
John Lu <johnlu2006@gmail.com>
2023-10-18 11:52:12 -0500

    update UserProfile class based on ERD

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -1,24 +1,59 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 
 namespace UrbanNest.Models;
-
 public class UserProfile
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(50)]
     public string LastName { get; set; }
+
+    [NotMapped] // NotMapped: ER Core will not create column for this property in DB
+    public string UserName { get; set; }
+
+    [NotMapped]
+    public string Email { get; set; }
+
     public string Address { get; set; }
 
-    [NotMapped] // not mapped means that EF Core won't create column for this property in the db
-    public string Email { get; set; }
-    [NotMapped]
-    public string UserName { get; set; }
-    [NotMapped]
-    public List<string> Roles { get; set; }
+    public string City { get; set; }
+
+    public string State { get; set; }
+
+    public string ZipCode { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public DateTime JoinedDate { get; set; }
+
+    public bool IsActive { get; set; }
+
+    [DataType(DataType.Url)]
+    [MaxLength(255)]
+    public string AvatarLocation { get; set; }
 
     public string IdentityUserId { get; set; }
 
     public IdentityUser IdentityUser { get; set; }
 
+    [NotMapped]
+    public List<string> Roles { get; set; }
+
+
+    public string FullName
+    {
+        get
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+    public List<Investor> Investors { get; set; }
+    public List<Agent> Agents { get; set; }
 }
