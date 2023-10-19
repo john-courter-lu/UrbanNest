96256c44ead7b456837302abc8007f573f70fe9b
John Lu <johnlu2006@gmail.com>
2023-10-19 14:46:02 -0500

    add and update UserProfileController

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,85 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UrbanNest.Data;
+using UrbanNest.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace UrbanNest.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserProfileController : ControllerBase
+{
+    private UrbanNestDbContext _dbContext;
+
+    public UserProfileController(UrbanNestDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext.UserProfiles.ToList());
+    }
+
+    // get UserProfiles with roles, as well as email and userName from IdentityUser, keep all the properties from UserProfile
+    [HttpGet("withroles")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult GetWithRoles()
+    {
+        return Ok(_dbContext.UserProfiles
+        .Include(up => up.IdentityUser)
+        .Select(up => new UserProfile
+        {
+            Id = up.Id,
+            FirstName = up.FirstName,
+            LastName = up.LastName,
+            Address = up.Address,
+            //need to add more like Zipcode, JoinedDate, isActive
+            Email = up.IdentityUser.Email,
+            UserName = up.IdentityUser.UserName,
+            IdentityUserId = up.IdentityUserId,
+            Roles = _dbContext.UserRoles
+            .Where(ur => ur.UserId == up.IdentityUserId)
+            .Select(ur => _dbContext.Roles.SingleOrDefault(r => r.Id == ur.RoleId).Name)
+            .ToList()
+        }));
+    }
+    // The query gets user profiles, then searches for user roles associated with the profile, and maps each of those to role names.
+
+    //add promote and demote endpoints
+    [HttpPost("promote/{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult Promote(string id)
+    {
+        IdentityRole role = _dbContext.Roles.SingleOrDefault(r => r.Name == "Admin");
+        // This will create a new row in the many-to-many UserRoles table.
+        _dbContext.UserRoles.Add(new IdentityUserRole<string>
+        {
+            RoleId = role.Id,
+            UserId = id
+        });
+        _dbContext.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpPost("demote/{id}")]
+    [Authorize(Roles = "Admin")]
+    public IActionResult Demote(string id)
+    {
+        IdentityRole role = _dbContext.Roles
+            .SingleOrDefault(r => r.Name == "Admin");
+        IdentityUserRole<string> userRole = _dbContext
+            .UserRoles
+            .SingleOrDefault(ur =>
+                ur.RoleId == role.Id &&
+                ur.UserId == id);
+
+        _dbContext.UserRoles.Remove(userRole);
+        _dbContext.SaveChanges();
+        return NoContent();
+    }
+}
