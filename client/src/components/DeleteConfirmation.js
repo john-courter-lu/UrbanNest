f89d18629d69da28af0b9d251cf69126ea868e62
John Lu <johnlu2006@gmail.com>
2023-10-24 22:10:27 -0500

    update styling

--- client/src/components/DeleteConfirmation.js
+++ client/src/components/DeleteConfirmation.js
@@ -1,20 +1,23 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 
 export default function DeleteConfirmation({ open, handleClose, handleDelete }) {
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Confirmation</DialogTitle>
-      <DialogContent>
-        <DialogContentText>Are you sure you want to delete this property?</DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose} color="secondary">
-          Cancel
-        </Button>
-        <Button onClick={handleDelete} color="primary">
-          Delete
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Confirmation</DialogTitle>
+
+            <DialogContent>
+                <DialogContentText>Are you sure you want to delete this property?</DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={handleClose} color="secondary">
+                    Cancel
+                </Button>
+                <Button onClick={handleDelete} color="secondary">
+                    Delete
+                </Button>
+            </DialogActions>
+
+        </Dialog>
+    );
 }
--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { Snackbar, SnackbarContent } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 
 export default function Notification({ message, onClose }) {
-  return (
-    <Snackbar
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={!!message}
-      autoHideDuration={3000}
-      onClose={onClose}
-    >
-      <SnackbarContent message={message} />
-    </Snackbar>
-  );
+    return (
+        <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={!!message}
+            autoHideDuration={3000}
+            onClose={onClose}
+        >
+            
+            {/* <SnackbarContent message={message} /> */}
+            {/* This is the way to show vanilla notifications */}
+
+            <Alert severity="success" onClose={onClose} sx={{ width: '100%' }}>
+                {message}
+            </Alert>
+        </Snackbar>
+    );
 }
