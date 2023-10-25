e716090be4c7a9a1c73179ab9bd239328f851e02
John Lu <johnlu2006@gmail.com>
2023-10-25 11:16:04 -0500

    update handleDelete to make API calls and update user view

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -1,11 +1,12 @@
 import Place from "@mui/icons-material/Place";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { Box, Stack, Card, CardMedia, CardContent, Menu, MenuItem, IconButton, Typography, useTheme, Dialog } from '@mui/material';
-import { Link } from "react-router-dom";
+import { Box, Stack, Card, CardMedia, CardContent, Menu, MenuItem, IconButton, Typography, useTheme } from '@mui/material';
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from 'react';
 import { tokens } from "../theme.js";
-import DeleteConfirmation from './DeleteConfirmation'; // Import your confirmation dialog
-import Notification from './Notification'; // Import your notification component
+import DeleteConfirmation from './DeleteConfirmation'; // Import confirmation dialog
+import Notification from './Notification'; // Import notification component
+import { deleteProperty } from "../managers/propertyManager.js";
 
 
 const PropertyCard = ({ id, title, location, type, photo, }) => {
@@ -19,6 +20,9 @@ const PropertyCard = ({ id, title, location, type, photo, }) => {
     // For DeleteConfirmation and Notification
     const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
     const [notificationMessage, setNotificationMessage] = useState('');
+
+    // For Handling Delete and Setting the card invisible
+    const [isVisible, setIsVisible] = useState(true);
 
     // Handle MoreVertIcon's menu
     const handleMenuOpen = (event) => {
@@ -40,101 +44,105 @@ const PropertyCard = ({ id, title, location, type, photo, }) => {
 
     // Handle Delete
     const handleDelete = () => {
-        // Perform the deletion (e.g., an API call)
-        // Replace this with your actual data deletion function
-        // For now, just log a message
-        console.log('XXX is deleted');
-
-        // Display a notification
-        setNotificationMessage('XXX is deleted');
-
         // Close the delete dialog
         closeDeleteDialog();
+
+        // Perform the deletion / an API call
+        // And make the user not see the card
+        deleteProperty(id)
+            .then(setIsVisible(false))
+            .then(() => {
+                // Display a notification
+                setNotificationMessage('This property is deleted');
+            })
     };
 
     return (
-        <Card
-            sx={{
-                width: "480px",
-                padding: "10px",
-                "&:hover": {
-                    boxShadow: `0 4px 20px 2px ${colors.primary[300]}`
-                },
-                backgroundColor: colors.primary[400],
-            }}
-            elevation={1} // 0.5 is not available
-        >
-
-            {photo &&
-                <Link to={`/properties/${id}`} sx={{ textDecoration: "none" }}>
-                    <CardMedia
-                        component="img"
-                        width="100%"
-                        height={240}
-                        image={photo}
-                        alt="card image"
-                        sx={{ borderRadius: "10px" }}
-                    />
-                </Link>}
-
-            <CardContent
+        <>
+            {isVisible && <Card
                 sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    gap: "10px",
-                    paddingX: "5px",
+                    width: "480px",
+                    padding: "10px",
+                    "&:hover": {
+                        boxShadow: `0 4px 20px 2px ${colors.primary[300]}`
+                    },
+                    backgroundColor: colors.primary[400],
                 }}
+                elevation={1} // 0.5 is not available
             >
-                <Stack direction="column" gap={1}>
-                    <Typography fontSize={16} fontWeight={500} color={colors.grey[100]}>
-                        {title}
-                    </Typography>
 
-                    <Stack direction="row" gap={0.5} alignItems="flex-start">
-                        <Place
-                            sx={{
-                                fontSize: 14,
-                                color: colors.grey[100],
-                                marginTop: 0.5,
-                            }}
+                {photo &&
+                    <Link to={`/properties/${id}`} sx={{ textDecoration: "none" }}>
+                        <CardMedia
+                            component="img"
+                            width="100%"
+                            height={240}
+                            image={photo}
+                            alt="card image"
+                            sx={{ borderRadius: "10px" }}
                         />
+                    </Link>}
 
-                        <Typography fontSize={14} color={colors.grey[100]}>
-                            {location}
-                        </Typography>
-                    </Stack>
-                </Stack>
-
-                <Box
-                    px={1.5}
-                    py={0.5}
-                    borderRadius={1}
-                    bgcolor={colors.primary[400]}
-                    height="fit-content"
+                <CardContent
+                    sx={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        gap: "10px",
+                        paddingX: "5px",
+                    }}
                 >
-                    <Typography fontSize={12} fontWeight={600} color="#6870fa">
-                        {type}
-                    </Typography>
-                </Box>
+                    <Stack direction="column" gap={1}>
+                        <Typography fontSize={16} fontWeight={500} color={colors.grey[100]}>
+                            {title}
+                        </Typography>
 
-                <Stack>
-                    <IconButton onClick={handleMenuOpen}>
-                        <MoreVertIcon />
-                    </IconButton>
+                        <Stack direction="row" gap={0.5} alignItems="flex-start">
+                            <Place
+                                sx={{
+                                    fontSize: 14,
+                                    color: colors.grey[100],
+                                    marginTop: 0.5,
+                                }}
+                            />
 
-                    <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={handleMenuClose}>
-                        <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
-                        <MenuItem onClick={openDeleteDialog}>Delete</MenuItem>
-                    </Menu>
-                </Stack>
-            </CardContent>
-            
+                            <Typography fontSize={14} color={colors.grey[100]}>
+                                {location}
+                            </Typography>
+                        </Stack>
+                    </Stack>
+
+                    <Box
+                        px={1.5}
+                        py={0.5}
+                        borderRadius={1}
+                        bgcolor={colors.primary[400]}
+                        height="fit-content"
+                    >
+                        <Typography fontSize={12} fontWeight={600} color="#6870fa">
+                            {type}
+                        </Typography>
+                    </Box>
+
+                    <Stack>
+                        <IconButton onClick={handleMenuOpen}>
+                            <MoreVertIcon />
+                        </IconButton>
+
+                        <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={handleMenuClose}>
+                            <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
+                            <MenuItem onClick={openDeleteDialog}>Delete</MenuItem>
+                        </Menu>
+                    </Stack>
+                </CardContent>
+
+
+            </Card>}
+
             {/* Child Components: */}
             <DeleteConfirmation open={isDeleteDialogOpen} handleClose={closeDeleteDialog} handleDelete={handleDelete} />
             <Notification message={notificationMessage} onClose={() => setNotificationMessage('')} />
-
-        </Card>
+        </>
     );
 };
 
