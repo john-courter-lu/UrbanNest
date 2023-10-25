a97254ef6784e03840c22f342f2b497a17a34ecf
John Lu <johnlu2006@gmail.com>
2023-10-24 19:54:38 -0500

    add MoreVertIcon for edit and delete features

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -1,45 +1,50 @@
 import Place from "@mui/icons-material/Place";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box, Stack, Card, CardMedia, CardContent, Menu, MenuItem, IconButton, Typography, useTheme } from '@mui/material';
 import { Link } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Stack from "@mui/material/Stack";
-import { useTheme } from "@mui/material";
+import { useState } from 'react';
 import { tokens } from "../theme.js";
+
 
 const PropertyCard = ({ id, title, location, type, photo, }) => {
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
+    const [menuAnchor, setMenuAnchor] = useState(null);
+
+    const handleMenuOpen = (event) => {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setMenuAnchor(null);
+    };
+
     return (
         <Card
-            component={Link}
-            to={`/properties/${id}`}
             sx={{
-                maxWidth: "330px",
+                width: "480px",
                 padding: "10px",
                 "&:hover": {
                     boxShadow: `0 4px 20px 2px ${colors.primary[300]}`
                 },
-                cursor: "pointer",
-                textDecoration: "none", // Remove underline
                 backgroundColor: colors.primary[400],
             }}
             elevation={1} // 0.5 is not available
         >
 
             {photo &&
-                <CardMedia
-                    component="img"
-                    width="100%"
-                    height={210}
-                    image={photo}
-                    alt="card image"
-                    sx={{ borderRadius: "10px" }}
-                />}
+                <Link to={`/properties/${id}`} sx={{ textDecoration: "none" }}>
+                    <CardMedia
+                        component="img"
+                        width="100%"
+                        height={240}
+                        image={photo}
+                        alt="card image"
+                        sx={{ borderRadius: "10px" }}
+                    />
+                </Link>}
 
             <CardContent
                 sx={{
@@ -82,6 +87,16 @@ const PropertyCard = ({ id, title, location, type, photo, }) => {
                     </Typography>
                 </Box>
 
+                <Stack>
+                    <IconButton onClick={handleMenuOpen}>
+                        <MoreVertIcon />
+                    </IconButton>
+
+                    <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={handleMenuClose}>
+                        <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
+                        <MenuItem onClick={handleMenuClose}>Delete</MenuItem>
+                    </Menu>
+                </Stack>
             </CardContent>
         </Card>
     );
