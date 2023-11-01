e8be01ee8e32607d95f1f3c90129ef6c0c3a5038
John Lu <johnlu2006@gmail.com>
2023-10-31 20:49:55 -0500

    make search bar show the value of searchTerm even when changed somewhere else

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -11,7 +11,7 @@ import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import LogoutIcon from '@mui/icons-material/Logout';
 
-const Topbar = ({ loggedInUser, setLoggedInUser, setSearchTerm }) => {
+const Topbar = ({ loggedInUser, setLoggedInUser, searchTerm, setSearchTerm }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     const colorMode = useContext(ColorModeContext);
@@ -30,6 +30,7 @@ const Topbar = ({ loggedInUser, setLoggedInUser, setSearchTerm }) => {
                 <InputBase
                     sx={{ ml: 2, flex: 1 }}
                     placeholder="Search"
+                    value={searchTerm} // If searchTerm is changed somewhere else, like when "2 Properties" is clicked in the Agent Stack, the InputBase will show the text. Otherwise, it'll be empty.
                     onChange={(e) => {
                         setSearchTerm(e.target.value)
                     }}
